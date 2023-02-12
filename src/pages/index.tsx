c5e94ddfd73be64956426f9181bda3d0d2ad72d1
Swank <willmadedesign@gmail.com>
2023-02-12 11:53:59 -0800

    Update config files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+  <div className="h-screen w-screen flex flex-col justify-center">
+    <div className="text-2xl text-center">Which Pokémon is roundest?</div>
+    <div className="border rounded p-8 flex justify-between">
+      <div className="w-16 h-16 bg-red-200"/>
+      <div>Vs</div>
+      <div className="w-16 h-16 bg-red-200"/>
+    </div>
+  </div>
+  );
 }
