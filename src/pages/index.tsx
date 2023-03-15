edb40a614808e71656f8f8fad3c4e8c914592ab1
Swank <willmadedesign@gmail.com>
2023-03-14 18:19:12 -0700

    Add random number plchlder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
-import { api } from "@/utils/api";
+//import { api } from "@/utils/api";
+
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
 export default function Home() {
-  const { data, isLoading } = api.example.hello.useQuery({ text: "Swank"});
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>;
-
+  const [first, second] = getOptionsForVote();
     return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is roundest?</div>
       <div className="p-2 "/>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-blue-500"/>
+        <div className="w-16 h-16 bg-blue-500">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-green-500"/>
+        <div className="w-16 h-16 bg-green-500">{second}</div>
       </div>
     </div>
     );
-}  
+}
